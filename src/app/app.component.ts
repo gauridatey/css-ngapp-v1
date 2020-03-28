c7e574d461b0dab247bad010fbb7509f9207b4f0
Manisha <manisha_msathe@yahoo.com>
2020-03-28 14:46:11 +0530

    internationalization related changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'css-ngapp-v1';
+  todayDate: Date = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe, 'de');
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HealthCheckModule, SharedModule],
